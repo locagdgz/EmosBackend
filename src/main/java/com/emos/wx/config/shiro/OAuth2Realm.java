7cbdd3aa6ab0af659cb92cdb896367496788ccf7
locadesktop <locagdgz@gmail.com>
2021-02-18 01:53:22 +0800

    完成授权和认证的具体逻辑

--- src/main/java/com/emos/wx/config/shiro/OAuth2Realm.java
+++ src/main/java/com/emos/wx/config/shiro/OAuth2Realm.java
@@ -1,5 +1,9 @@
 package com.emos.wx.config.shiro;
 
+import com.emos.wx.db.dao.TbUserDao;
+import com.emos.wx.db.pojo.TbUser;
+import com.emos.wx.exception.EmosException;
+import com.emos.wx.service.UserService;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
@@ -11,10 +15,20 @@ import org.apache.shiro.subject.PrincipalCollection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.Set;
+
+/**
+ * 认证流程：
+ *  小程序->XssFilter->OAuth2Filter->[OAuth2Realm认证]->OAuth2Realm授权->Controller
+ */
 @Component
 public class OAuth2Realm extends AuthorizingRealm {
     @Autowired
     private JwtUtil jwtUtil;
+
+    @Autowired
+    private UserService userService;
+
     @Override
     public boolean supports(AuthenticationToken token) {
         return token instanceof OAuth2Token;
@@ -25,8 +39,15 @@ public class OAuth2Realm extends AuthorizingRealm {
     @Override
     protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
         SimpleAuthorizationInfo info = new SimpleAuthorizationInfo();
-        //TODO 查询用户的权限列表
-        //TODO 把权限列表添加到info对象中
+        // 查询用户的权限列表
+        /**
+         * @see #doGetAuthenticationInfo(AuthenticationToken) 中可以看到在info对象中添加了user对象，所以这里可以获取到
+         */
+        TbUser user = (TbUser) principals.getPrimaryPrincipal();
+        Integer id = user.getId();
+        // 把权限列表添加到info对象中
+        Set<String> permissions = userService.searchUserPermissions(id);
+        info.setStringPermissions(permissions);
         return info;
     }
     /**
@@ -34,9 +55,15 @@ public class OAuth2Realm extends AuthorizingRealm {
      */
     @Override
     protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
-        //TODO 从令牌中获取userId，然后检测该账户是否被冻结。
-        SimpleAuthenticationInfo info = new SimpleAuthenticationInfo();
-        //TODO 往info对象中添加用户信息、Token字符串
-        return info;
+        // 从令牌中获取userId，然后检测该账户是否被冻结。
+        String accessToken = (String) token.getPrincipal();
+        int userId = jwtUtil.getUserId(accessToken);
+        TbUser tbUser = userService.searchUserById(userId);
+        if (tbUser != null) {
+            // 往info对象中添加用户信息、Token字符串
+            return new SimpleAuthenticationInfo(tbUser, accessToken, getName());
+        } else {
+            throw new EmosException("用户不存在或已冻结，请联系管理员");
+        }
     }
 }
