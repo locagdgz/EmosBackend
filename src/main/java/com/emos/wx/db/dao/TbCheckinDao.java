9ce63e829f4a9acecabcbab51f5a675f6d8006e4
locadesktop <locagdgz@gmail.com>
2021-02-18 01:55:12 +0800

    判断是否可以签到的sql

--- src/main/java/com/emos/wx/db/dao/TbCheckinDao.java
+++ src/main/java/com/emos/wx/db/dao/TbCheckinDao.java
@@ -1,9 +1,12 @@
 package com.emos.wx.db.dao;
 
-import com.emos.wx.db.pojo.TbCheckin;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.Map;
 
 @Mapper
 public interface TbCheckinDao {
+
+    Integer haveCheckin(Map map);
 
 }
