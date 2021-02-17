fdfe352454bcd2c31201ac27196b515be3a86e02
locadesktop <locagdgz@gmail.com>
2021-02-18 01:57:23 +0800

    增加注释

--- src/main/java/com/emos/wx/config/xss/XssFilter.java
+++ src/main/java/com/emos/wx/config/xss/XssFilter.java
@@ -5,6 +5,9 @@ import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
+/**
+ * Xss过滤器，具体过滤逻辑由RequestWrapper实现
+ */
 @WebFilter(urlPatterns = "/*")
 public class XssFilter implements Filter {
     public void init(FilterConfig config) throws ServletException {
