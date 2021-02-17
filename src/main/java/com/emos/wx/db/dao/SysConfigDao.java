3957c45aec1fce55fc1cea2ca0a38c1fbf31697a
locadesktop <locagdgz@gmail.com>
2021-02-18 01:54:50 +0800

    读取签到时间配置的sql

--- src/main/java/com/emos/wx/db/dao/SysConfigDao.java
+++ src/main/java/com/emos/wx/db/dao/SysConfigDao.java
@@ -3,7 +3,11 @@ package com.emos.wx.db.dao;
 import com.emos.wx.db.pojo.SysConfig;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface SysConfigDao {
+
+    List<SysConfig> selectAllParam();
 
 }
