ffe1997815b47b7001ba3b448803be779ac49300
locadesktop <locagdgz@gmail.com>
2021-02-18 01:55:30 +0800

    判断是否节假日的sql

--- src/main/java/com/emos/wx/db/dao/TbHolidaysDao.java
+++ src/main/java/com/emos/wx/db/dao/TbHolidaysDao.java
@@ -1,9 +1,10 @@
 package com.emos.wx.db.dao;
 
-import com.emos.wx.db.pojo.TbHolidays;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface TbHolidaysDao {
+
+    Integer searchTodayIsHolidays();
 
 }
