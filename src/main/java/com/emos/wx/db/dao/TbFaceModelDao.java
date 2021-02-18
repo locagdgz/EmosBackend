2ee17496ce4cd782d1714350ce82bbc7d67a160e
locadesktop <locagdgz@gmail.com>
2021-02-18 22:36:41 +0800

    新增根据UserId查找人脸模型SQL;人脸模型Insert SQL;根据UserId删除人脸模型SQL

--- src/main/java/com/emos/wx/db/dao/TbFaceModelDao.java
+++ src/main/java/com/emos/wx/db/dao/TbFaceModelDao.java
@@ -6,4 +6,8 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface TbFaceModelDao {
 
+    String searchFaceModel(int userId);
+    void insert(TbFaceModel faceModelEntity);
+    int deleteFaceModel(int userId);
+
 }
