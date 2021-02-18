e7603d7f2e89830816854d68d97b7c913fce6e19
locadesktop <locagdgz@gmail.com>
2021-02-18 22:33:35 +0800

    新增Checkin Insert SQL

--- src/main/java/com/emos/wx/db/dao/TbCheckinDao.java
+++ src/main/java/com/emos/wx/db/dao/TbCheckinDao.java
@@ -1,5 +1,6 @@
 package com.emos.wx.db.dao;
 
+import com.emos.wx.db.pojo.TbCheckin;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.Map;
@@ -8,5 +9,7 @@ import java.util.Map;
 public interface TbCheckinDao {
 
     Integer haveCheckin(Map map);
+
+    void insert(TbCheckin entity);
 
 }
